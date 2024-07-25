ce523883d7ffad5a15da0c4c59da98889c3f5d52
Paula Cristina Engler Ribeiro <153039428+paulaengler@users.noreply.github.com>
2024-07-25 10:30:07 -0300

    modal

--- Exercicios-Semana8/src/App.jsx
+++ Exercicios-Semana8/src/App.jsx
@@ -5,21 +5,62 @@ import Hero from './components/Hero'
 import Produtos from './components/Produtos'
 import PaginaProdutos from './pages/PaginaProdutos'
 import PaginaLogin from './pages/PaginaLogin'
+import Modal from 'react-modal';
+import { useState } from 'react'
 
 
 function App() {
 
+  const customStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+    },
+  };
+
+  const [modalIsOpen, setIsOpen] = useState(true)
+ 
+  function handleOpenModal (){
+    setIsOpen(true)
+    alert('Você não possui idade suficiente para acessar esta página.')
+  }
+
+ 
+  function handleCloseModal(){
+    setIsOpen(false)
+    
+  }
+
 
   return (
     <>
-    {/* <Modal></Modal> */}
-     <BrowserRouter>
-        <Routes>
+
+         <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={() => handleCloseModal('nao')}
+          contentLabel="Example Modal" 
+          style={customStyles}    
+        >
+          <h2>Possui idade acima de 18 anos? </h2>
+          <button onClick={handleCloseModal}>Sim</button>
+          <button onClick={handleOpenModal}>Não</button>
+          
+        </Modal>
+
+
+
+        <BrowserRouter>        
+        <Routes>          
           <Route path='/' Component={Header} />
           <Route path='/signin' Component={PaginaLogin} />
           <Route path='/produtos' Component={PaginaProdutos} />
         </Routes>
       </BrowserRouter>
+      
       <Hero/>
       <Produtos/>
     </>
--- Exercicios-Semana8/src/components/Modal.jsx
+++ Exercicios-Semana8/src/components/Modal.jsx
@@ -1,3 +1,54 @@
-import React from 'react';
-import Modal from 'react-modal';
+// import Modal from 'react-modal';
+// import { useState } from 'react'
 
+// Modal.setAppElement('#root')
+
+// function Modal() {
+
+//     const [modalIsOpen, setIsOpen] = useState(true)
+  
+//     function handleOpenModal (){
+//       setIsOpen(true)
+//     }
+
+//     function afterOpenModal() {
+//         // references are now sync'd and can be accessed.
+//         subtitle.style.color = '#f00';
+//       }
+  
+//     function handleCloseModal(){
+//       setIsOpen(false)
+//     }
+  
+//     return (
+//         <>
+//         <div className="modal">
+//           <button className="btn-modal" onClick={handleOpenModal}> Modal </button>
+//         </div>
+
+//         <Modal
+//           isOpen={modalIsOpen}
+//           onAfterOpen={afterOpenModal}
+//           onRequestClose={handleCloseModal}
+//           contentLabel="Example Modal"          
+//         >
+//           <h2>Hello</h2>
+//           <button onClick={handleCloseModal}>Close</button>
+//           <h3> i am a modal </h3>
+//         </Modal>
+        
+//         </>
+
+//     )
+//     }
+
+// export default Modal
+
+
+ // if (acao === 'sim') {
+    //   // Lógica para redirecionar o usuário para a página desejada
+    //   window.location.href = '/home';
+    // } else if (acao === 'nao') {
+    //   // Lógica para gerar um alerta
+    //   alert('Você não possui idade suficiente para acessar esta página.');
+    // }
